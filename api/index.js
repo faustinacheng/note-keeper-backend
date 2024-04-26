c1a45da1b5e5e3c30ae40f3622adb5d570c47a83
Faustina <83921854+faustinacheng@users.noreply.github.com>
2024-04-26 06:16:00 -0400

    setting Headers for cors

--- api/index.js
+++ api/index.js
@@ -19,6 +19,14 @@ app.get("/", (req, res) => {
 });
 
 app.get("/notes/:id", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+    );
     Note.find({ uid: req.params.id }) // Find all notes
         .exec()
         .then((notes) => {
@@ -33,6 +41,14 @@ app.get("/notes/:id", (req, res) => {
 
 // POST
 app.post("/notes/", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+    );
     const note = new Note({
         _id: new mongoose.Types.ObjectId(),
         title: req.body.title,
