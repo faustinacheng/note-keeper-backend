c689ce5421bb4973bc09978bc0bda5526d2b939d
Faustina <83921854+faustinacheng@users.noreply.github.com>
2024-05-03 20:11:31 -0400

    Changed app.options

--- api/index.js
+++ api/index.js
@@ -76,7 +76,17 @@ app.get("/notes/:id", (req, res) => {
         });
 });
 
-app.options("/notes/", cors(corsOptions));
+app.options("/notes/", (req, res) => {
+    // res.setHeader("Access-Control-Allow-Origin", "*");
+    // res.setHeader("Access-Control-Allow-Credentials", "true");
+    // res.setHeader("Access-Control-Max-Age", "1800");
+    // res.setHeader("Access-Control-Allow-Headers", "content-type");
+    // res.setHeader(
+    //     "Access-Control-Allow-Methods",
+    //     "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+    // );
+    res.status(200).send();
+});
 
 // POST
 app.post("/notes/", (req, res) => {
