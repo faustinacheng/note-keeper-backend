ec9a0c23c60ed21f8b01bf7f549001e9a4d69afb
Faustina <83921854+faustinacheng@users.noreply.github.com>
2024-05-04 15:02:20 -0400

    Changed cors origin to vercel url

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ const app = express();
 mongoose.connect(process.env.MONGO_DB_URI);
 
 const corsOptions = {
-    origin: true,
+    origin: "https://keeper-fc.vercel.app",
     methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     credentials: true, //access-control-allow-credentials:true
